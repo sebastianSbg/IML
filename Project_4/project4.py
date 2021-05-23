082bb2553d12603c029cfdf7eeef9b232330ccbb
sebastianSbg <bommers@student.ethz.ch>
2021-05-22 19:02:26 -0700

    cleaned up version

--- Project_4/project4.py
+++ Project_4/project4.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
-"""main_project4_7.ipynb
+"""
 
+The networks trains a siamese network, which is used to predict similarity between different dishes.
 Created by Sebastian Bommer 2021_05_19
 
+TODO: Improve Architecture and use pretrained blocks
+TODO: ADD GPU CAPABILITY and perform Computing on CCP or AWS
+TODO: Add visualization
+- make image size inputs consistent
+
 """
-# import cPickle
+
 import time
 import datetime
 import math
@@ -13,45 +19,89 @@ import pandas as pd
 import _pickle as pickle
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # 0,1.2,3
 import keras
 import tensorflow as tf
 import cv2
-
 import random
 from sklearn.model_selection import train_test_split
 from keras.models import load_model
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # 0,1.2,3
 
-# # from google.colab import files
-# import io
-# import time
+"""FUNCTION AND CLASS DEFINITIONS"""
 
-# import pdb
+# def plot_triplets(examples, imgSize):
+#     plt.figure(figsize=(6, 2))
+#     for i in range(3):
+#         plt.subplot(1, 3, 1 + i)
+#         plt.imshow(np.reshape(examples[i], (imgSize[1], imgSize[0], 3)))
+#         plt.xticks([])
+#         plt.yticks([])
+#     plt.show()
 
-# import matplotlib.pyplot as plt
+def myLogger(logLevel, fileLogLevel = logging.WARNING):
+    path = os.path.dirname(__file__)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logLevel)
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+    file_handler_e = logging.FileHandler(f'log.log')
+    file_handler_e.setLevel(fileLogLevel)
+    file_handler_e.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler_e)
+    logger.addHandler(stream_handler)
+    return logger
 
-# import keras.backend as K
-# from keras import Sequential
+# Preprocessing function
+def get_pp_images(img_size = [100,100], maxImages=10000, img_path=None,  force_recompute=False, savePP = True):  # TODO: Improve reading images with LMDB and HDF5
+    """ Function applies downsampling and preprocessing to original images. The processed data can be saved in a pickel item and reloaded for future runs."""
 
-# from keras.layers import Dense, Dropout, Activation, SimpleRNN, LSTM, Conv1D, MaxPooling1D, AveragePooling1D, Embedding
-# from keras.layers import BatchNormalization
-# from keras.layers import AlphaDropout
-# from keras.layers import Conv2D, MaxPooling2D, Flatten, Lambda, Input, Layer
+    out_dic = {}
 
-# from keras.regularizers import l2
+    if img_path == None:
+        pp_path = os.path.join('data', 'img_dic.pickle')  # preprocessed pickle object path
+    else:
+        pp_path = img_path
 
-# from keras.models import Model
+    if os.path.exists(pp_path) and not force_recompute:
+        logger.info("Loading preprocessed data ... ")
+        try:
+            with open(pp_path, "rb") as input_file:
+                pickle_in = pickle.load(input_file)
+                return pickle_in
+            logger.info("The image array was successfully loaded")
+        except:
+            logger.error("The image array failed to load")
+    else:
+        logger.info("Computing preprocessed data .. ")
+        idx = 0
+        img_name = lambda idx: os.path.join('data', 'food', f'{idx:05d}.jpg')
+        while True and idx < maxImages:
+            if idx%500==0:logger.debug(f"Image name: {img_name(idx)}")
+            try:
+                if not os.path.exists(img_name(idx)):
+                    raise FileExistsError
 
-# from keras import regularizers
-# from keras.optimizers import SGD
-# from keras.callbacks import Callback,ModelCheckpoint, EarlyStopping
-# from keras import initializers
+                img = cv2.imread(img_name(idx))  # resize image
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # resize image
+                img = cv2.resize(img, img_size)  # appending image
+                out_dic[idx] = img
 
-from matplotlib import pyplot as plt
+                idx += 1
 
+            except:
+                logger.exception(f"File does not exist, import was terminated at index {idx}")
+                break
 
-"""FUNCTION DEFINITIONS"""
+        if savePP:
+            try:
+                with open(pp_path, "wb") as output_file:
+                    pickle.dump(out_dic, output_file)
+                logger.info("The image array was successfully saved!")
+            except:
+                logger.exception("The image array failed to save!")
 
+    return out_dic
 
 def similar_dish(prediction):
     out = [0] * len(prediction)
@@ -65,106 +115,6 @@ def similar_dish(prediction):
         else:
             out[idx] = 0
     return out
-
-
-# # less memory intense way of creating predictions
-# def predict_vec(fold, batch_size=1000, toSize=(450, 300), flatten=True):
-#     if flatten:
-#         x_anchors = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
-#         x_positives = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
-#         x_negatives = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
-#     else:
-#         x_anchors = np.zeros([batch_size, toSize[1], toSize[0], 3])
-#         x_positives = np.zeros([batch_size, toSize[1], toSize[0], 3])
-#         x_negatives = np.zeros([batch_size, toSize[1], toSize[0], 3])
-#     predictions = []
-#     c = 0
-#     totlen = 0
-#     # get random samples from train triplets
-#     for i in range(0, len(fold)):
-#         if i % 500 == 0:
-#             print(f'The progress is {round(i / len(fold) * 100, 1)} %')
-#         # preprocessing
-#         # We need to find an anchor, a positive example and a negative example
-#         triplet = fold[i]
-#         # reshaping, normalizing and flattening images --> this will take more time than pre-processed data, but will eliminate RAM issues
-#         x_anchor = cv2.resize(food_images[triplet[0]], toSize) / 255.0
-#         x_positive = cv2.resize(food_images[triplet[1]], toSize) / 255.0
-#         x_negative = cv2.resize(food_images[triplet[2]], toSize) / 255.0
-#         if flatten:
-#             x_anchor = np.reshape(x_anchor, np.prod(x_anchor.shape))
-#             x_positive = np.reshape(x_positive, np.prod(x_positive.shape))
-#             x_negative = np.reshape(x_negative, np.prod(x_negative.shape))
-#         x_anchors[c] = x_anchor
-#         x_positives[c] = x_positive
-#         x_negatives[c] = x_negative
-#         if c >= (batch_size - 1) or i == (len(fold) - 1):
-#             if i == (len(fold) - 1):
-#                 print(f'End with c {c}')
-#             print(f'i is {i}, Predicting...')
-#             # print(x_anchors[-1])
-#             X = [x_anchors[0:c], x_positives[0:c], x_negatives[0:c]]
-#             print(np.array(X).shape)
-#             vec = net.predict(X)
-#             predictions.append(similar_dish(vec))
-#             # predictions.append(similar_dish(vec))
-#             print('End of predictions...')
-#             totlen = totlen + len(vec)
-#             # resetting c
-#             c = -1
-#             # break
-#         c = c + 1
-#     pred_flat = []
-#     for li in predictions:
-#         for elem in li:
-#             pred_flat.append(elem)
-
-    # print(f' The length of the vector is: {totlen}')
-    # return pred_flat
-
-
-
-# def run_through(fold, toSize=(450, 300), flatten=True):
-#     batch_size = len(fold)
-#     if flatten:
-#         x_anchors = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
-#         x_positives = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
-#         x_negatives = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
-#     else:
-#         x_anchors = np.zeros([batch_size, toSize[1], toSize[0], 3])
-#         x_positives = np.zeros([batch_size, toSize[1], toSize[0], 3])
-#         x_negatives = np.zeros([batch_size, toSize[1], toSize[0], 3])
-
-#     # get random samples from
-
-# for i in range(0, len(fold)):
-#     # preprocessing
-#     # We need to find an anchor, a positive example and a negative example
-#     triplet = fold[i]
-#     # reshaping, normalizing and flattening images --> this will take more time than pre-processed data, but will eliminate RAM issues
-#     x_anchor = cv2.resize(food_images[triplet[0]], toSize) / 255.0
-#     x_positive = cv2.resize(food_images[triplet[1]], toSize) / 255.0
-#     x_negative = cv2.resize(food_images[triplet[2]], toSize) / 255.0
-#     if flatten:
-#         x_anchor = np.reshape(x_anchor, np.prod(x_anchor.shape))
-#         x_positive = np.reshape(x_positive, np.prod(x_positive.shape))
-#         x_negative = np.reshape(x_negative, np.prod(x_negative.shape))
-
-    # x_anchors[i] = x_anchor
-    # x_positives[i] = x_positive
-    # x_negatives[i] = x_negative
-    # # yield [x_anchors, x_positives, x_negatives]
-
-# return [x_anchors, x_positives, x_negatives]
-
-# def plot_triplets(examples, imgSize):
-#     plt.figure(figsize=(6, 2))
-#     for i in range(3):
-#         plt.subplot(1, 3, 1 + i)
-#         plt.imshow(np.reshape(examples[i], (imgSize[1], imgSize[0], 3)))
-#         plt.xticks([])
-#         plt.yticks([])
-#     plt.show()
 
 def create_batch(fold, img_dict, batch_size=16, toSize=(100, 100), flatten=False):
     if flatten:
@@ -212,70 +162,6 @@ def data_generator(fold, img_dict, batch_size=4, toSize=(100,100), flatten=True)
         # logger.debug(f"Generator called with shape {np.array(X).shape}")
         yield X, y
 
-def get_pp_images(img_size = [100,100], maxImages=10000, img_path=None,  force_recompute=False, savePP = True):  # TODO: Improve reading images with LMDB and HDF5
-    """ Function applies downsampling and preprocessing to original images. The processed data can be saved in a pickel item and reloaded for future runs."""
-
-    out_dic = {}
-
-    if img_path == None:
-        pp_path = os.path.join('data', 'img_dic.pickle')  # preprocessed pickle object path
-    else:
-        pp_path = img_path
-
-    if os.path.exists(pp_path) and not force_recompute:
-        logger.info("Loading preprocessed data ... ")
-        try:
-            with open(pp_path, "rb") as input_file:
-                pickle_in = pickle.load(input_file)
-                return pickle_in
-            logger.info("The image array was successfully loaded")
-        except:
-            logger.error("The image array failed to load")
-    else:
-        logger.info("Computing preprocessed data .. ")
-        idx = 0
-        img_name = lambda idx: os.path.join('data', 'img', f'{idx:05d}.jpg')
-        while True and idx < maxImages:
-            if idx%500==0:logger.debug(f"Image name: {img_name(idx)}")
-            try:
-                if not os.path.exists(img_name(idx)):
-                    raise FileExistsError
-
-                img = cv2.imread(img_name(idx))  # resize image
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # resize image
-                img = cv2.resize(img, img_size)  # appending image
-                out_dic[idx] = img
-
-                idx += 1
-
-            except:
-                logger.exception(f"File does not exist, import was terminated at index {idx}")
-                break
-
-        if savePP:
-            try:
-                with open(pp_path, "wb") as output_file:
-                    pickle.dump(out_dic, output_file)
-                logger.info("The image array was successfully saved!")
-            except:
-                logger.exception("The image array failed to save!")
-
-    return out_dic
-
-def myLogger(logLevel, fileLogLevel = logging.WARNING):
-    path = os.path.dirname(__file__)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logLevel)
-    formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-    file_handler_e = logging.FileHandler(f'log.log')
-    file_handler_e.setLevel(fileLogLevel)
-    file_handler_e.setFormatter(formatter)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
-    logger.addHandler(file_handler_e)
-    logger.addHandler(stream_handler)
-    return logger
-
 def triplet_loss(y_true, y_pred):
    anchor, positive, negative = y_pred[:, :SIZE_BOTTLENECK], y_pred[:, SIZE_BOTTLENECK:2 * SIZE_BOTTLENECK], y_pred[:, 2 * SIZE_BOTTLENECK:]
    positive_dist = tf.reduce_mean(tf.square(anchor - positive), axis=1)
@@ -319,13 +205,16 @@ class siameseNetwork():
         self.net.summary()
         tf.keras.utils.plot_model(self.net, show_shapes=True, show_layer_names=True)
 
+    def compileModel(self):
+        self.net.compile(loss=triplet_loss, optimizer='adamax', metrics=[tf.keras.metrics.Accuracy, tf.keras.metrics.Precision])  # TODO: Change optimizer to 'adamax' + define custom loss to be triplet loss
+
     def train(self, data_in, img_dict, epochs = 20, batch_size = 8, saveModel = True):
         # keras.losses.custom_loss = self.triplet_loss
         # if loss:
         #     loss = self.triplet_loss()
-        keras.losses.custom_loss = triplet_loss
+        # keras.losses.custom_loss = triplet_loss
 
-        self.net.compile(loss = triplet_loss, optimizer = 'adamax')  #TODO: Change optimizer to 'adamax' + define custom loss to be triplet loss
+        # self.net.compile(loss = triplet_loss, optimizer = 'adamax')  #TODO: Change optimizer to 'adamax' + define custom loss to be triplet loss
         try:
             self.net.fit(data_generator(data_in, img_dict, batch_size = batch_size, flatten=False), epochs=epochs, steps_per_epoch = math.floor(data_in.shape[0] / batch_size))
             self.timeCompleted = int(time.time())
@@ -358,7 +247,9 @@ class siameseNetwork():
 
     def inference(self, data, imgDict, savePredictions = True, path = None):
         # generator to avoid having to load everything into memory
-        inferenceGenerator = ([np.array([imgDict[triplet[0]]/255.0]), np.array([imgDict[triplet[1]]/255.0]), np.array([imgDict[triplet[2]]/255.0])] for triplet in data)  #TODO: Numpy array conversions to be done in preprocessing steps
+        imgDict_corrected = {key: np.array([item/255.0]) for key, item in imgDict.items()}
+        # inferenceGenerator = ([np.array([imgDict[triplet[0]]/255.0]), np.array([imgDict[triplet[1]]/255.0]), np.array([imgDict[triplet[2]]/255.0])] for triplet in data)
+        inferenceGenerator = ([imgDict_corrected[triplet[0]], imgDict_corrected[triplet[1]], imgDict_corrected[triplet[2]]] for triplet in data)  #TODO: Generate batch sizes to help with speed and consider shuffling data
 
         # logger.debug(f"{next(inferenceGenerator)}")
 
@@ -373,8 +264,7 @@ class siameseNetwork():
             logger.debug(f"Predict input shape: {np.array(X).shape}")
             logger.debug(f"Type of my input {type(X)}")
             logger.debug(f"Image shape: {(imgDict[0]/255.0).shape}")
-            logger.debug(f"inference shape: {np.array(next(inferenceGenerator)).shape}")
-            self.predictions = self.net.predict_generator(inferenceGenerator)
+            self.predictions = self.net.predict(inferenceGenerator)
             # self.net.predict(X)
         except:
             logger.exception(f"Inference failed!")
@@ -391,25 +281,23 @@ class siameseNetwork():
         try:
             sv_pr = similar_dish(self.predictions)
             np.savetxt(file_path, sv_pr, fmt='%i')
-            logger.info("The predictions were successfully saved ... ")
+            logger.info("The predictions were successfully saved.")
         except:
             logger.exception("The predictions couldn't be saved!")
 
 
-    def loadLatestModel(self):
+    def loadLatestModel(self, compile = True):
         model_path = os.path.join('models', max(os.listdir('./models')))
-        logger.info(f"Model Path: {model_path}")
         try:
-            self.net = load_model(model_path, custom_objects={"triplet_loss": triplet_loss})
+            self.net = load_model(model_path, custom_objects={"triplet_loss": triplet_loss}, compile=compile)
             self.net.summary()
             logger.info(f"Successfully loaded in model {model_path}")
         except FileNotFoundError:
 
             logger.error(f"Model was not found: {model_path} does not exist")
         except:
-            logger.exception(f"Latest Model could not be loaded!")
+            logger.exception(f"Latest Model could not be loaded from {model_path}!")
         return self.net
-
 
 if __name__ == "__main__":
 
@@ -418,18 +306,17 @@ if __name__ == "__main__":
     SIZE_BOTTLENECK = 50
     ALPHA = 1.0 # margin for triplet loss
     BATCH_SIZE = 64
-    EPOCHS = 10
-    LOG_LEVEL = logging.DEBUG
+    EPOCHS = 1
+    LOG_LEVEL = logging.INFO
     INPUT_ON = True # if False the user can intervene using inputs
-    TEST_SIZE = 0.2
+    TEST_SIZE = 0.05
+    PRETRAINED_LATEST_MODEL = False
 
     """Importing and preprocessing"""
 
     logger = myLogger(LOG_LEVEL)
 
-    img_dic = get_pp_images(maxImages=10000, force_recompute=False)
-
-    # """## Importing training and testing triplets"""
+    img_dic = get_pp_images(maxImages=10000, force_recompute=False)  #If you have issues with the preprocessing set force_recompute to True
 
     try:
         X_df = pd.read_csv(os.path.join('data', 'train_triplets.txt'), header = None, delim_whitespace=True)
@@ -439,40 +326,38 @@ if __name__ == "__main__":
     except FileNotFoundError:
         logger.error(".txt file was not found!")
     except:
-        logger.exception("Couldn't read the .txt file")
+        logger.exception("Couldn't read the .txt file!")
 
-    # train_triplets = np.array(X_train_df)
-    # test_triplets = np.array(X_test_df)
-    # n_train = np.shape(X_train_df)[0]
-    # t_test = np.shape(X_test_df)[0]
-
-    X = np.array(X_df)  # TODO: create a validation set
+    X = np.array(X_df)
     X_pred = np.array(X_predict_df)
     y = np.ones(X.shape[0])
 
     X_train, X_val, y_train, y_val= train_test_split(X, y, test_size=TEST_SIZE, random_state=1)
 
     if INPUT_ON and input("Do you want to perform inference only?: YES for INFERENCE Y/N: ").lower() in ['y','yes','1']:
-        # TODO: import model and create inference
         logger.info(f"INFERENCE ONLY ... ")
         net = siameseNetwork(IMG_SIZE_IN)
         net.loadLatestModel()
 
         net.inference(X_pred, img_dic, savePredictions=True)
+
+        logger.info(f"Inference ended.")
     else:
         logger.info(f"TRAINING and INFERENCE ... ")
 
         net = siameseNetwork(IMG_SIZE_IN)
-        net.get_model_summary()
-        net.train(X_train, img_dic, epochs=EPOCHS)
 
-        # file_path = os.path.join('results', f"results_{int(time.time())}")
-        # predicted_embeddings = net.inference(X_pred, img_dic)
-        # sv_pr = similar_dish(predicted_embeddings)
-        # np.savetxt(file_path, sv_pr, fmt='%i', )
+        # Choose whether to train where we left off or wheter to use new weights and retrain model
+        if PRETRAINED_LATEST_MODEL:
+            net.loadLatestModel(compile=True)
+            # net.compileModel()
+            net.train(X_train, img_dic, epochs=EPOCHS)
+        else:
+            net.compileModel()
+            net.train(X_train, img_dic, epochs=EPOCHS)
 
-        # dg = data_generator(X_train, img_dic)
-        # logger.debug(len(next(dg)))
+        net.inference(X_pred, img_dic, savePredictions=True)
+
         logger.info(f"Training and Inference ended.")
 
 
