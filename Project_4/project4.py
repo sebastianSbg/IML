b8a8a5e46c773227ce48a6f564efc80be60536b8
sebastianSbg <bommers@student.ethz.ch>
2021-05-22 22:22:39 -0700

    added tensorboard and validaton parameter to fit function

--- Project_4/project4.py
+++ Project_4/project4.py
@@ -4,13 +4,15 @@
 The networks trains a siamese network, which is used to predict similarity between different dishes.
 Created by Sebastian Bommer 2021_05_19
 
-TODO: Improve Architecture and use pretrained blocks
+TODO: Improve Architecture and use pretrained blocks to improve accuracy
 TODO: ADD GPU CAPABILITY and perform Computing on CCP or AWS
 TODO: Add visualization
-- make image size inputs consistent
+TODO: Add evaluation net.evaluate(X,y)
 
 """
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # 0,1.2,3
 import time
 import datetime
 import math
@@ -18,14 +20,14 @@ import logging
 import pandas as pd
 import _pickle as pickle
 import numpy as np
-import os
 import keras
 import tensorflow as tf
 import cv2
 import random
 from sklearn.model_selection import train_test_split
 from keras.models import load_model
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # 0,1.2,3
+from tensorflow.keras.callbacks import TensorBoard
+
 
 """FUNCTION AND CLASS DEFINITIONS"""
 
@@ -53,7 +55,7 @@ def myLogger(logLevel, fileLogLevel = logging.WARNING):
     return logger
 
 # Preprocessing function
-def get_pp_images(img_size = [100,100], maxImages=10000, img_path=None,  force_recompute=False, savePP = True):  # TODO: Improve reading images with LMDB and HDF5
+def get_pp_images(img_size = [100,100], maxImages=10000, img_path=None,  force_recompute=False, savePP = True):
     """ Function applies downsampling and preprocessing to original images. The processed data can be saved in a pickel item and reloaded for future runs."""
 
     out_dic = {}
@@ -116,9 +118,9 @@ def similar_dish(prediction):
             out[idx] = 0
     return out
 
-def create_batch(fold, img_dict, batch_size=16, toSize=(100, 100), flatten=False):
+def create_batch(fold, img_dict, batch_size=16, flatten=False):
+    toSize = img_dict[list(img_dict.keys())[0]].shape
     if flatten:
-        # toSize = img_dict[0][0].shape
         x_anchors = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
         x_positives = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
         x_negatives = np.zeros((batch_size, toSize[0] * toSize[1] * 3))
@@ -126,38 +128,35 @@ def create_batch(fold, img_dict, batch_size=16, toSize=(100, 100), flatten=False
         x_anchors = np.zeros([batch_size, toSize[1], toSize[0], 3])
         x_positives = np.zeros([batch_size, toSize[1], toSize[0], 3])
         x_negatives = np.zeros([batch_size, toSize[1], toSize[0], 3])
-    # get random samples from train triplets
+
+    # get random samples from training set
     for i in range(0, batch_size):
-        # preprocessing
-        # We need to find an anchor, a positive example and a negative example
         random_index = random.randint(0, fold.shape[0] - 1)
         triplet = fold[random_index]
-        # logger.debug(f"Triplet: {triplet}")
-        # reshaping, normalizing and flattening images --> this will take more time than pre-processed data, but will eliminate RAM issues
+
         try:
-            # x_anchor = cv2.resize(img_dict[int(triplet[0])], toSize) / 255.0
             x_anchor = img_dict[int(triplet[0])] / 255.0
-            # x_positive = cv2.resize(img_dict[int(triplet[1])], toSize) / 255.0
             x_positive = img_dict[int(triplet[1])] / 255.0
-            # x_negative = cv2.resize(img_dict[int(triplet[2])], toSize) / 255.0
             x_negative = img_dict[int(triplet[2])]/ 255.0
         except KeyError:
             logger.exception(f"One of the keys was not found in the image dictionary {int(triplet[0])}, {int(triplet[1])}, {int(triplet[2])} not found")
         except:
             logger.exception("Error getting and resizing images")
+
         if flatten:
             x_anchor = np.reshape(x_anchor, np.prod(x_anchor.shape))
             x_positive = np.reshape(x_positive, np.prod(x_positive.shape))
             x_negative = np.reshape(x_negative, np.prod(x_negative.shape))
+
         x_anchors[i] = x_anchor
         x_positives[i] = x_positive
         x_negatives[i] = x_negative
         # logger.debug(f"X_anchor shape: {x_anchors.shape}")
-    return [x_anchors, x_positives, x_negatives]  # TODO: remove np.array conversion?
+    return [x_anchors, x_positives, x_negatives]
 
-def data_generator(fold, img_dict, batch_size=4, toSize=(100,100), flatten=True):
+def data_generator(fold, img_dict, batch_size=4, flatten=True):
     while True:
-        X = create_batch(fold, img_dict, batch_size, toSize, flatten)
+        X = create_batch(fold, img_dict, batch_size, flatten)
         y = np.zeros((batch_size, 3 * SIZE_BOTTLENECK))
         # logger.debug(f"Generator called with shape {np.array(X).shape}")
         yield X, y
@@ -206,21 +205,32 @@ class siameseNetwork():
         tf.keras.utils.plot_model(self.net, show_shapes=True, show_layer_names=True)
 
     def compileModel(self):
-        self.net.compile(loss=triplet_loss, optimizer='adamax', metrics=[tf.keras.metrics.Accuracy, tf.keras.metrics.Precision])  # TODO: Change optimizer to 'adamax' + define custom loss to be triplet loss
+        self.net.compile(loss=triplet_loss, optimizer='adamax' , metrics=[tf.keras.metrics.Accuracy(), tf.keras.metrics.Precision()])
 
-    def train(self, data_in, img_dict, epochs = 20, batch_size = 8, saveModel = True):
-        # keras.losses.custom_loss = self.triplet_loss
-        # if loss:
-        #     loss = self.triplet_loss()
-        # keras.losses.custom_loss = triplet_loss
+    def train(self, data_in, img_dict, data_in_val = None, epochs = 20, batch_size = 8, saveModel = True, modelName=None):
 
-        # self.net.compile(loss = triplet_loss, optimizer = 'adamax')  #TODO: Change optimizer to 'adamax' + define custom loss to be triplet loss
         try:
-            self.net.fit(data_generator(data_in, img_dict, batch_size = batch_size, flatten=False), epochs=epochs, steps_per_epoch = math.floor(data_in.shape[0] / batch_size))
+            if modelName == None:
+                tensorboard = TensorBoard(log_dir=f'logs/{int(time.time())}.log')
+            else:
+                tensorboard = TensorBoard(log_dir=f'logs/{modelName}.log')
+
+            if not data_in_val.any():
+                logger.debug("Training NOT using validation set.")
+                self.net.fit(data_generator(data_in, img_dict, batch_size = batch_size, flatten=False,), callbacks=[tensorboard], epochs=epochs, steps_per_epoch = math.floor(data_in.shape[0] / batch_size))
+            else:
+                logger.debug("Training using validation set.")
+                imgDict_corrected = {key: np.array([item / 255.0]) for key, item in img_dict.items()}
+                dg_val= (([imgDict_corrected[triplet[0]], imgDict_corrected[triplet[1]], imgDict_corrected[triplet[2]]],np.zeros((1, 3 * SIZE_BOTTLENECK))) for triplet in data_in_val)
+                dg_train = data_generator(data_in, img_dict, batch_size=batch_size, flatten=False)
+                # self.net.fit(dg_train, validation_data = dg_val, validation_steps = 500, callbacks=[tensorboard], epochs=epochs, steps_per_epoch = math.floor(data_in.shape[0] / batch_size))
+                self.net.fit(dg_train, validation_data = dg_val, validation_steps = 500, callbacks=[tensorboard], epochs=epochs, steps_per_epoch = math.floor(data_in.shape[0] / batch_size))
+
             self.timeCompleted = int(time.time())
             readableTime = str(datetime.datetime.fromtimestamp(self.timeCompleted).strftime('%c'))
             logger.info(f'Finished training at {readableTime}')
         except:
+            self.timeCompleted = int(time.time())
             file_path = os.path.join('models', f"interrupted_model_{self.timeCompleted}.h5")
             self.net.save_weights(file_path)
             logger.exception("Siamese Network failed to train!")
@@ -249,7 +259,7 @@ class siameseNetwork():
         # generator to avoid having to load everything into memory
         imgDict_corrected = {key: np.array([item/255.0]) for key, item in imgDict.items()}
         # inferenceGenerator = ([np.array([imgDict[triplet[0]]/255.0]), np.array([imgDict[triplet[1]]/255.0]), np.array([imgDict[triplet[2]]/255.0])] for triplet in data)
-        inferenceGenerator = ([imgDict_corrected[triplet[0]], imgDict_corrected[triplet[1]], imgDict_corrected[triplet[2]]] for triplet in data)  #TODO: Generate batch sizes to help with speed and consider shuffling data
+        inferenceGenerator = ([imgDict_corrected[triplet[0]], imgDict_corrected[triplet[1]], imgDict_corrected[triplet[2]]] for triplet in data)
 
         # logger.debug(f"{next(inferenceGenerator)}")
 
@@ -306,11 +316,12 @@ if __name__ == "__main__":
     SIZE_BOTTLENECK = 50
     ALPHA = 1.0 # margin for triplet loss
     BATCH_SIZE = 64
-    EPOCHS = 1
+    EPOCHS = 50
     LOG_LEVEL = logging.INFO
     INPUT_ON = True # if False the user can intervene using inputs
-    TEST_SIZE = 0.05
-    PRETRAINED_LATEST_MODEL = False
+    TEST_SIZE = 0.1
+    PRETRAINED_LATEST_MODEL = True  # If True it will use the weights of the latest model
+    NAME = f"Siamese_Network_{int(time.time())}"
 
     """Importing and preprocessing"""
 
@@ -351,112 +362,11 @@ if __name__ == "__main__":
         if PRETRAINED_LATEST_MODEL:
             net.loadLatestModel(compile=True)
             # net.compileModel()
-            net.train(X_train, img_dic, epochs=EPOCHS)
+            net.train(X_train, img_dic, epochs=EPOCHS, modelName=NAME, data_in_val=X_val)
         else:
             net.compileModel()
-            net.train(X_train, img_dic, epochs=EPOCHS)
+            net.train(X_train, img_dic, epochs=EPOCHS, modelName=NAME, data_in_val=X_val)
 
         net.inference(X_pred, img_dic, savePredictions=True)
 
         logger.info(f"Training and Inference ended.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# """# Test predictions & save results"""
-
-# # optionally loading a model
-# # from tensorflow.keras.models import load_model
-
-# # inp = input('Select model to run: ')
-
-# # net_saved = load_model(inp, custom_objects={'triplet_loss': triplet_loss})
-
-# # function for transforming predicted embeddings to required prediction 0 or 1
-# # anchor: dish A, positive: dish B, negative: dish C
-# # if distance A to B < A to C then A is more similar to B than to C -> return 1
-
-# #--> debug predict_vec (the output sizes are off by 1)
-# # pr_val = predict_vec(validate, batch_size = 501, toSize = IMG_SIZE_IN, flatten=False)
-
-# # # There is still a discrepancy
-# # print(len(outVec))
-# # print(len(validate))
-
-# # # percentage accuracy
-# # perc_accuracy = round(np.sum(pr_val)/len(pr_val)*100,1)
-# # print(f'The accurate predictions from the validation set is {perc_accuracy} %')
-
-# #deprecated
-
-# # X = run_through(validate, toSize = IMG_SIZE_IN, flatten=False)
-
-# # predicted_embeddings = net.predict(X)
-# # predicted_embeddings.shape
-
-# # making predictions for the test set in the first fold
-# # sv_f1 = [similar_dish(emb) for emb in predicted_embeddings]
-# # perc_accurate = round(np.sum(sv_f1)/len(sv_f1)*100,1)
-# # print(f'The percentage of accurate predictions for the first fold test set is: {perc_accurate} %')
-
-# # pr_test = predict_vec(D_test, batch_size = 1000, toSize = IMG_SIZE_IN, flatten=False)
-
-# # # percentage accuracy
-# # positive_given = round(np.sum(pr_test)/len(pr_test)*100,1)
-# # print(f'Predicted positive images given is {positive_given} %')
-
-# # deprecated
-# D_test = np.array(D_test)
-# X = run_through(D_test, toSize = IMG_SIZE_IN, flatten=False)
-
-# predicted_embeddings = net.predict(X)
-# predicted_embeddings.shape
-
-# # # making predictions for the test set in the first fold
-# sv_pr = similar_dish(predicted_embeddings)
-# perc_accurate = round(np.sum(sv_pr)/len(sv_pr)*100,1)
-# print(f'The percentage of accurate predictions for the test set is: {perc_accurate} %')
-# print(np.array(sv_pr).shape)
-
-# description = input('Add an optional description to the result file: ')
-# filepath = f'{workDir}/results/{round(now.timestamp())}_sb_result_{description}.txt'
-# np.savetxt(filepath, sv_pr, fmt='%i', )
---
+++ Project_4/readme.txt
@@ -0,0 +1,4 @@
+1) Extract the data in the directory and rename it data
+2) create a models folder
+3) create a results folder
+4) run the script and work on TODO comments
